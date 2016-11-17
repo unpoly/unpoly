637abb715a2627c729ee596c03b8814f32ff884b
Henning Koch <henning.koch@makandra.de>
2016-11-17 13:14:19 +0100

    No dot in package.json "main" key; Version 0.31.2

--- lib/unpoly/rails/version.rb
+++ lib/unpoly/rails/version.rb
@@ -4,6 +4,6 @@ module Unpoly
     # The current version of the unpoly-rails gem.
     # This version number is also used for releases of the Unpoly
     # frontend code.
-    VERSION = '0.31.1'
+    VERSION = '0.31.2'
   end
 end
