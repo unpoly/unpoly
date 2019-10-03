237d757545eb18ab39c2f1d7d45dc4a1056db43e
Henning Koch <henning.koch@makandra.de>
2019-10-03 16:08:21 +0200

    WIP. I hate Webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,81 @@
-// var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-
-// console.log("--------------")
-// console.log(process.env.NODE_ENV)
-// console.log(process.env.WEBPACK_MODE)
-// console.log(process.env)
-// console.log("--------------")
-
-let erbLoader = {
-  loader: 'rails-erb-loader',
-  options: {
-    runner: 'ruby',
-    engine: 'erb'
-  }
-}
-
-let tsLoader = {
-  loader: 'ts-loader',
-  options: {
-    appendTsSuffixTo: [/.*/],
-    transpileOnly: true,
-    compilerOptions: {
-      allowJs: true,
-      checkJs: true,
-      module: "ESNext",
-      target: "ESNext"
+function scriptPipeline(target) {
+  let erbLoader = {
+    loader: 'rails-erb-loader',
+    options: {
+      runner: 'ruby',
+      engine: 'erb'
     }
+  }
 
+  let tsLoader = {
+    loader: 'ts-loader',
+    options: {
+      appendTsSuffixTo: [/.*/],
+      transpileOnly: true,
+      compilerOptions: {
+        allowJs: true,
+        checkJs: true,
+        module: "ESNext",
+        target: target
+      }
+
+    }
+  }
+
+  let coffeeLoader = {
+    loader: 'coffee-loader'
+  }
+
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: [tsLoader]
+        },
+        {
+          test: /\.coffee$/,
+          exclude: /node_modules/,
+          use: [tsLoader, coffeeLoader]
+        },
+        {
+          test: /\.js\.erb$/,
+          exclude: /node_modules/,
+          use: [tsLoader, erbLoader]
+        },
+        {
+          test: /\.coffee\.erb$/,
+          exclude: /node_modules/,
+          use: [tsLoader, coffeeLoader, erbLoader]
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.js', '.coffee', '.js.erb', '.coffee.erb']
+    }
   }
 }
 
-let coffeeLoader = {
-  loader: 'coffee-loader'
+function file(srcPath, outputFilename) {
+  return {
+    entry: srcPath,
+    output: {
+      path: __dirname + '/dist',
+      // publicPath: '/',
+      filename: outputFilename,
+    },
+    devServer: {
+      contentBase: './dist'
+    }
+  }
 }
+
+
+let es5Pipeline = scriptPipeline('ES5')
+
+let esNextPipeline = scriptPipeline('ESNext')
+
 
 
 module.exports = {
