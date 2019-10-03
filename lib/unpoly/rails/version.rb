b2d571a83df8342bce9f82548e696f0ac0fe99cd
Henning Koch <henning.koch@makandra.de>
2019-10-03 14:00:55 +0200

    Track current library version in package.json instead of Unpoly::Rails::VERSION

--- lib/unpoly/rails/version.rb
+++ lib/unpoly/rails/version.rb
@@ -1,9 +1,5 @@
 module Unpoly
   module Rails
-    ##
-    # The current version of the unpoly-rails gem.
-    # This version number is also used for releases of the Unpoly
-    # frontend code.
-    VERSION = '0.61.0'
+    VERSION = JSON.parse(File.read('package.json')).fetch('version')
   end
 end
