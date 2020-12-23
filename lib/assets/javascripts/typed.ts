cf24f4635d38415397120c2572936d672674132a
Henning Koch <henning.koch@makandra.de>
2020-12-23 12:36:18 +0100

    Experiment more with TypeScript

--- lib/assets/javascripts/typed.ts
+++ lib/assets/javascripts/typed.ts
@@ -1,64 +1,75 @@
 // https://www.typescriptlang.org/docs/handbook/declaration-files/templates/global-d-ts.html
+// https://www.typescriptlang.org/docs/handbook/unions-and-intersections.html
+// https://typescript-play.js.org/#code/PTAEAsBdIBwZwFwgO6oHSQJ4wKZwMYBOAljJADYCGAdgOZoD2htwAJg-nMODawEYMGAazY58VQpUjEG1ALQAzYuTzBIOALYwq6rrXIM+lcnNZzIcNFA3kAUCAjR4SYKmQZseIqQo16TFnZObl4BYWAAV2oZajg5XjlianVCODFpWUtrO1tWMQkcUAiYUABeUABvAF9bW2pKDTwYSnxC4srbUFBO0CxcUABRFUbk4jhwMsHhnGTQAB9QOEgSOlqauoamlraYNBVaFswOrpwADxgmSFAFKPwM6lBCGc3WAAVCBlxCLAAKQwArdIIUAAeT4gLuABpQAxyKwANI4TDApYrWjQ6g4ZCI5GLZZJWgASg6PRq6w2jTgzVaRV2EWk5BJXSSKQU21AACU8DhIJQ+Cpjl1HtzIMCfsTSgA+UAANwYxFYpNqJ3Ol2ut3uoA0lCEOAAwrIlLQADwAFUlP35ERwMBWotA4rK0tNhOBptAADJOSK+QKKj0uk9IBFCA9-UKhWgo1abXbxZCA0Knml7TdqHcYo7qomal0ybUCxStjT2v6emcLt9QOJKHA4KADdQjWbpeGhfhMssIncmJbyNbbSyxRLncSyxH1hHkzys8qhbnQPmCyorvgWuBCuVMchaWhG0afmmM7Is8Lg6HKtdBMCAOQKQQ3xdVQm1DuxK4RSbFND05QAblsN8llATddxwaZkgAosqXZb9JFYGQmVAFkcEINkaQ5SgEIYfdiFoQUunAKJaEITAAGVwPSJhEDxNEAG0AF1EwuchyAASWSVCZWMYFqAiDQ+FQpVy1VKsgNXQ08K-Bk0G1XVcJNTDsIUi1HSlAizxDMNE0I4jSIolQe1SYE6JvOjijkIi6FIhibwYhMIy6Fj2M4whuPIYEAGYAAZfO8nMALzQlAt3fZDjQJ56kaN4Pi+X43yNaEbyskjMBvJKUv0yijLgG9grnI9NWnSBTwSvCIpFcU-0XAqNRiCA9PI7LIF7MdEyDLTq0k+hMqawyWtSNB-nlagfnSvKQsnEDRKuFC0PZMjeW+V5YXIAkQTIGJ6zbZlXPcgB+Xj+MEwhE1kABFa1rVYQ7QAAMTq2RhJVSsrkK+qlkoZbVoJH5KJGe0hk0GZpHGaFPnuRBFq+yAVtY9bNsySZqjaiMV2QvbjEmCGtrQOb3NAfb9pAtA+IE1CAEFoEIP6IMgJKLPx4w8sJ4myvoZyOJSdyQq6dHpEaQhJj41jedAdHCCiaI6EmLscDF9GqCWLkAEdrWArcIlFxMzsR2JGGoS6cGuyZ3pPConjVvBIGfDS+drSBVfVq5ykt52xaqKCIzNh52C5AwsNnRzkIUB0xjhta6FeSgIjSVhxVR4PQHYMj8A3VgtZwKqdMXEDyDSO2hQiNBjGQShMDgH44PAhhA-+kHwb1uBCWhFO05wDOVHy4OFzzMXEx95OGFT9PM6DxziFDn5JeoaWiULroBdQyYU1NYhGgYekfj9musOhJnyG7xze5qxM29HlRxTnQMeU6n3x6nKWCVN4w0mqhwABFZBvK4zjEelCiQA3LuUu5dK7Eh4NQVgKhTrB0ng6JWjscBW2AgAQk1qxROwdEFO2tiXAQ3wqqgAcBTAhVwaBHFwFA5+btrY5xPl0cQOAvprw3lvJehBgrELAItT4oBKCLHbp3DuvR15CQnEqLoPRB7hx+lHGOccH6aQvEEfiIMrAKjyNQSaXtdwdi0MoVCY1zIwDkM5Wy0JPrfXhnQfKPRvwnhvMUBAChJCNGQEwIQCBirpWFCmOxNQgA
 
-// const modalPackage = {
-//   fn: function() {}
-// }
 
-// modal.fn()
 
-// namespace up {
-//   const privateSymbol = "bar"
-//   export const modal = "foo"
-// }
+namespace up {
+  
+  export type Elementish = Element | string
+
+}
+
+namespace up.legacy {
+  export function renamedProperty(object: Object, oldKey: string, newKey: string) {
+
+  }
+}
+
+
+namespace up.util {
+
+  interface Resetable {
+    reset: () => void
+  }
+
+  export function makeConfig<T>(blueprint: () => T): T & Resetable {
+    return {
+      ...blueprint(),
+      reset: function() {}
+    }
+  }
+}
+
+
 
 
 
 namespace up {
 
-  function privateFunction() {
+  export class Config<T> {
+    constructor(blueprint: () => T) {
 
+    }
+
+    reset() {
+
+    }
   }
-
-  export function publicFunction() {
-
-  }
-
-}
-
-namespace up.radio {
-
-  publicFunction()
-
-  export function radioFunction() {
-
-  }
-
-}
-
-namespace up {
-  export const radioFunction = radio.radioFunction
 }
 
 
 
+let cache = new up.Config(function() { return { foo: 'foo' }})
 
-namespace up.foo {
-  namespace legacy {
+let fooo = Object.create(cache)
 
-  }
-  }
 
 
 const u = up.util;
 const e = up.element;
-
 
 let y = false
 let x = y ?? 22 ?? 33;
 
 namespace up.radio {
 
-  let config = new up.Config(() => {
+  interface RadioConfig {
+    hungrySelectors: string[]
+    pollInterval: number
+  }
+
+  export const config = util.makeConfig<RadioConfig>(() => {
     return {
       hungrySelectors: ['[up-hungry]'],
       pollInterval: 30000
@@ -69,29 +80,28 @@ namespace up.radio {
   function reset() { config.reset(); }
 
   function hungrySelector() {
-      config.hungrySelectors.join(',');
+    return config.hungrySelectors.join(',');
   }
 
   export interface StartPollingOptions {
     interval?: number
-    onQueued?: Function
   }
 
-  export function startPolling(element, options:StartPollingOptions = {}) {
+  export function startPolling(element: Elementish, options:StartPollingOptions = {}) {
     let interval = options.interval ?? e.numberAttr(element, 'up-interval') ?? config.pollInterval;
     let timer = null;
     let running = true;
     let lastRequest = null;
-    
-    options.onQueued = function({request}) {
+
+    let reloadOptions = { ...options, onQueued: function({request}) {
       lastRequest = request;
-    };
+    }}
 
     function doReload() {
       if (isPollingPaused()) {
         doSchedule();
       } else {
-        u.always(up.reload(element, options), doSchedule);
+        u.always(up.reload(element, reloadOptions), doSchedule);
       }
     };
     
@@ -109,8 +119,8 @@ namespace up.radio {
         lastRequest.abort(); // Abort any pending request
       }
       clearTimeout(timer); // Stop a scheduled timer
-    };
-  };
+    }
+  }
   
   function isPollingPaused() {
     return document.hidden;
@@ -120,12 +130,4 @@ namespace up.radio {
 
   up.on('up:framework:reset', reset);
 
-  return {
-    config: config,
-    hungrySelector: hungrySelector,
-    startPolling: startPolling
-  };
-})();
-
-
-
+}
