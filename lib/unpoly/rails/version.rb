fb97f856b23cbd7e4a25e727d0338b3098de2c3a
Henning Koch <henning.koch@makandra.de>
2021-07-13 11:37:47 +0200

    Version 2.1.0

--- lib/unpoly/rails/version.rb
+++ lib/unpoly/rails/version.rb
@@ -4,6 +4,6 @@ module Unpoly
     # The current version of the unpoly-rails gem.
     # This version number is also used for releases of the Unpoly
     # frontend code.
-    VERSION = '2.0.1'
+    VERSION = '2.1.0'
   end
 end
