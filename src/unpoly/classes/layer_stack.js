4659aa43e618358ee75cce19121239f281455e23
Henning Koch <henning.koch@makandra.de>
2021-07-30 12:27:24 +0200

    Decaffeinate up.H* and up.L* classes

---
+++ src/unpoly/classes/layer_stack.js
@@ -0,0 +1,159 @@
+const u = up.util
+const e = up.element
+
+up.LayerStack = class LayerStack extends Array {
+
+  constructor() {
+    super()
+    // When TypeScript transpiles to ES5, there is an issue with this constructor always creating
+    // a `this` of type `Array` instead of `LayerStack`. The transpiled code looks like this:
+    //
+    //     function LayerStack() {
+    //       let this = Array.call(this) || this
+    //     }
+    //
+    // And since Array() returns a value, this returns the new this.
+    // The official TypeScript recommendation is to use setProtoTypeOf() after calling super:
+    // https://github.com/Microsoft/TypeScript/wiki/FAQ#why-doesnt-extending-built-ins-like-error-array-and-map-work
+    Object.setPrototypeOf(this, up.LayerStack.prototype)
+    this.currentOverrides = []
+    this.push(this.buildRoot())
+  }
+
+  buildRoot() {
+    return up.layer.build({ mode: 'root', stack: this })
+  }
+
+  remove(layer) {
+    u.remove(this, layer)
+  }
+
+  peel(layer, options) {
+    // We will dismiss descendants closer to the front first to prevent
+    // recursive calls of peel().
+    const descendants = u.reverse(layer.descendants)
+
+    // Callers expect the effects of peel() to manipulate the layer stack sync.
+    // Because of this we will dismiss alle descendants sync rather than waiting
+    // for each descendant to finish its closing animation.
+    const dismissOptions = u.merge(options, {preventable: false})
+
+    for (let descendant of descendants) {
+      descendant.dismiss(':peel', dismissOptions)
+    }
+  }
+
+  reset() {
+    this.peel(this.root, {animation: false})
+    this.currentOverrides = []
+    this.root.reset()
+  }
+
+  isOpen(layer) {
+    return layer.index >= 0
+  }
+
+  isClosed(layer) {
+    return !this.isOpen(layer)
+  }
+
+  parentOf(layer) {
+    return this[layer.index - 1]
+  }
+
+  childOf(layer) {
+    return this[layer.index + 1]
+  }
+
+  ancestorsOf(layer) {
+    // Return closest ancestors first
+    return u.reverse(this.slice(0, layer.index))
+  }
+
+  selfAndAncestorsOf(layer) {
+    // Order for layer.closest()
+    return [layer, ...layer.ancestors]
+  }
+
+  descendantsOf(layer) {
+    return this.slice(layer.index + 1)
+  }
+
+  isRoot(layer) {
+    return this[0] === layer
+  }
+
+  isOverlay(layer) {
+    return !this.isRoot(layer)
+  }
+
+  isCurrent(layer) {
+    return this.current === layer
+  }
+
+  isFront(layer) {
+    return this.front === layer
+  }
+
+  get(...args) {
+    return this.getAll(...args)[0]
+  }
+
+  getAll(...args) {
+    return new up.LayerLookup(this, ...args).all()
+  }
+
+  sync() {
+    for (let layer of this) {
+      layer.sync()
+    }
+  }
+
+  asCurrent(layer, fn) {
+    try {
+      this.currentOverrides.push(layer)
+      return fn()
+    } finally {
+      this.currentOverrides.pop()
+    }
+  }
+
+  reversed() {
+    return u.reverse(this)
+  }
+
+  dismissOverlays(value = null, options = {}) {
+    options.dismissable = false
+    for (let overlay of u.reverse(this.overlays)) {
+      overlay.dismiss(value, options)
+    }
+  }
+
+  // Used by up.util.reverse() and specs
+  [u.copy.key]() {
+    return u.copyArrayLike(this)
+  }
+
+  get count() {
+    return this.length
+  }
+
+  get root() {
+    return this[0]
+  }
+
+  get overlays() {
+    return this.root.descendants
+  }
+
+  get current() {
+    // Event listeners and compilers will push into @currentOverrides
+    // to temporarily set up.layer.current to the layer they operate in.
+    return u.last(this.currentOverrides) || this.front
+  }
+
+  get front() {
+    return u.last(this)
+  }
+
+}
---
+++ src/unpoly/classes/link_preloader.js
@@ -0,0 +1,81 @@
+const u = up.util
+const e = up.element
+
+up.LinkPreloader = class LinkPreloader {
+
+  constructor() {
+    this.considerPreload = this.considerPreload.bind(this)
+  }
+
+  observeLink(link) {
+    // If the link has an unsafe method (like POST) and is hence not preloadable,
+    // prevent up.link.preload() from blowing up by not observing the link (even if
+    // the user uses [up-preload] everywhere).
+    if (up.link.isSafe(link)) {
+      this.on(link, 'mouseenter',           event => this.considerPreload(event, true))
+      this.on(link, 'mousedown touchstart', event => this.considerPreload(event))
+      this.on(link, 'mouseleave',           event => this.stopPreload(event))
+    }
+  }
+
+  on(link, eventTypes, callback) {
+    up.on(link, eventTypes, { passive: true }, callback)
+  }
+
+  considerPreload(event, applyDelay) {
+    const link = event.target
+    if (link !== this.currentLink) {
+      this.reset()
+
+      this.currentLink = link
+
+      // Don't preload when the user is holding down CTRL or SHIFT.
+      if (up.link.shouldFollowEvent(event, link)) {
+        if (applyDelay) {
+          this.preloadAfterDelay(link)
+        } else {
+          this.preloadNow(link)
+        }
+      }
+    }
+  }
+
+  stopPreload(event) {
+    if (event.target === this.currentLink) {
+      return this.reset()
+    }
+  }
+
+  reset() {
+    if (!this.currentLink) { return }
+
+    clearTimeout(this.timer)
+
+    // Only abort if the request is still preloading.
+    // If the user has clicked on the link while the request was in flight,
+    // and then unhovered the link, we do not abort the navigation.
+    if (this.currentRequest?.preload) {
+      this.currentRequest.abort()
+    }
+
+    this.currentLink = undefined
+    this.currentRequest = undefined
+  }
+
+  preloadAfterDelay(link) {
+    const delay = e.numberAttr(link, 'up-delay') ?? up.link.config.preloadDelay
+    this.timer = u.timer(delay, () => this.preloadNow(link))
+  }
+
+  preloadNow(link) {
+    // Don't preload if the link was removed from the DOM while we were waiting for the timer.
+    if (e.isDetached(link)) {
+      this.reset()
+      return
+    }
+
+    const onQueued = request => { return this.currentRequest = request; }
+    up.log.muteUncriticalRejection(up.link.preload(link, { onQueued }))
+    this.queued = true
+  }
+}
