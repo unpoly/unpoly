d9dcd789a619e538ffb41bfa69a069f18ff514b7
Henning Koch <henning.koch@makandra.de>
2021-08-02 08:50:26 +0200

    Fix doc indents in up.motion

--- src/unpoly/motion.js
+++ src/unpoly/motion.js
@@ -58,26 +58,26 @@ up.motion = (function() {
   const motionController = new up.MotionController('motion')
 
   /*-
-   Sets default options for animations and transitions.
+  Sets default options for animations and transitions.
 
-   @property up.motion.config
-   @param {number} [config.duration=175]
-   The default duration for all animations and transitions (in milliseconds).
-   @param {string} [config.easing='ease']
-   The default timing function that controls the acceleration of animations and transitions.
+  @property up.motion.config
+  @param {number} [config.duration=175]
+    The default duration for all animations and transitions (in milliseconds).
+  @param {string} [config.easing='ease']
+    The default timing function that controls the acceleration of animations and transitions.
 
-   See [MDN documentation](https://developer.mozilla.org/en-US/docs/Web/CSS/transition-timing-function)
-   for a list of pre-defined timing functions.
-   @param {boolean} [config.enabled]
-   Whether animation is enabled.
+    See [MDN documentation](https://developer.mozilla.org/en-US/docs/Web/CSS/transition-timing-function)
+    for a list of pre-defined timing functions.
+  @param {boolean} [config.enabled]
+    Whether animation is enabled.
 
-   By default animations are enabled, unless the user has configured their
-   system to [minimize non-essential motion](https://developer.mozilla.org/en-US/docs/Web/CSS/@media/prefers-reduced-motion).
+    By default animations are enabled, unless the user has configured their
+    system to [minimize non-essential motion](https://developer.mozilla.org/en-US/docs/Web/CSS/@media/prefers-reduced-motion).
 
-   Set this to `false` to disable animation globally.
-   This can be useful in full-stack integration tests.
-   @stable
-   */
+    Set this to `false` to disable animation globally.
+    This can be useful in full-stack integration tests.
+  @stable
+  */
   const config = new up.Config(() => ({
     duration: 175,
     easing: 'ease',
@@ -96,96 +96,96 @@ up.motion = (function() {
   }
 
   /*-
-   Returns whether Unpoly will perform animations.
+  Returns whether Unpoly will perform animations.
 
-   Set [`up.motion.config.enabled = false`](/up.motion.config#config.enabled) in order to disable animations globally.
+  Set [`up.motion.config.enabled = false`](/up.motion.config#config.enabled) in order to disable animations globally.
 
-   @function up.motion.isEnabled
-   @return {boolean}
-   @stable
-   */
+  @function up.motion.isEnabled
+  @return {boolean}
+  @stable
+  */
   function isEnabled() {
     return config.enabled
   }
 
   /*-
-   Applies the given animation to the given element.
+  Applies the given animation to the given element.
 
-   ### Example
+  ### Example
 
-   ```js
-   up.animate('.warning', 'fade-in')
-   ```
+  ```js
+  up.animate('.warning', 'fade-in')
+  ```
 
-   You can pass additional options:
+  You can pass additional options:
 
-   ```
-   up.animate('.warning', 'fade-in', {
-    delay: 1000,
-    duration: 250,
-    easing: 'linear'
+  ```js
+  up.animate('.warning', 'fade-in', {
+   delay: 1000,
+   duration: 250,
+   easing: 'linear'
   })
-   ```
+  ```
 
-   ### Named animations
+  ### Named animations
 
-   The following animations are pre-defined:
+  The following animations are pre-defined:
 
-   | `fade-in`          | Changes the element's opacity from 0% to 100% |
-   | `fade-out`         | Changes the element's opacity from 100% to 0% |
-   | `move-to-top`      | Moves the element upwards until it exits the screen at the top edge |
-   | `move-from-top`    | Moves the element downwards from beyond the top edge of the screen until it reaches its current position |
-   | `move-to-bottom`   | Moves the element downwards until it exits the screen at the bottom edge |
-   | `move-from-bottom` | Moves the element upwards from beyond the bottom edge of the screen until it reaches its current position |
-   | `move-to-left`     | Moves the element leftwards until it exists the screen at the left edge  |
-   | `move-from-left`   | Moves the element rightwards from beyond the left edge of the screen until it reaches its current position |
-   | `move-to-right`    | Moves the element rightwards until it exists the screen at the right edge  |
-   | `move-from-right`  | Moves the element leftwards from beyond the right  edge of the screen until it reaches its current position |
-   | `none`             | An animation that has no visible effect. Sounds useless at first, but can save you a lot of `if` statements. |
+  | `fade-in`          | Changes the element's opacity from 0% to 100% |
+  | `fade-out`         | Changes the element's opacity from 100% to 0% |
+  | `move-to-top`      | Moves the element upwards until it exits the screen at the top edge |
+  | `move-from-top`    | Moves the element downwards from beyond the top edge of the screen until it reaches its current position |
+  | `move-to-bottom`   | Moves the element downwards until it exits the screen at the bottom edge |
+  | `move-from-bottom` | Moves the element upwards from beyond the bottom edge of the screen until it reaches its current position |
+  | `move-to-left`     | Moves the element leftwards until it exists the screen at the left edge  |
+  | `move-from-left`   | Moves the element rightwards from beyond the left edge of the screen until it reaches its current position |
+  | `move-to-right`    | Moves the element rightwards until it exists the screen at the right edge  |
+  | `move-from-right`  | Moves the element leftwards from beyond the right  edge of the screen until it reaches its current position |
+  | `none`             | An animation that has no visible effect. Sounds useless at first, but can save you a lot of `if` statements. |
 
-   You can define additional named animations using [`up.animation()`](/up.animation).
+  You can define additional named animations using [`up.animation()`](/up.animation).
 
-   ### Animating CSS properties directly
+  ### Animating CSS properties directly
 
-   By passing an object instead of an animation name, you can animate
-   the CSS properties of the given element:
+  By passing an object instead of an animation name, you can animate
+  the CSS properties of the given element:
 
-   ```
-   var warning = document.querySelector('.warning')
-   warning.style.opacity = 0
-   up.animate(warning, { opacity: 1 })
-   ```
+  ```js
+  var warning = document.querySelector('.warning')
+  warning.style.opacity = 0
+  up.animate(warning, { opacity: 1 })
+  ```
 
-   CSS properties must be given in `kebab-case`, not `camelCase`.
+  CSS properties must be given in `kebab-case`, not `camelCase`.
 
-   ### Multiple animations on the same element
+  ### Multiple animations on the same element
 
-   Unpoly doesn't allow more than one concurrent animation on the same element.
+  Unpoly doesn't allow more than one concurrent animation on the same element.
 
-   If you attempt to animate an element that is already being animated,
-   the previous animation will instantly jump to its last frame before
-   the new animation begins.
+  If you attempt to animate an element that is already being animated,
+  the previous animation will instantly jump to its last frame before
+  the new animation begins.
 
-   @function up.animate
-   @param {Element|jQuery|string} element
-   The element to animate.
-   @param {string|Function(element, options): Promise|Object} animation
-   Can either be:
+  @function up.animate
+  @param {Element|jQuery|string} element
+    The element to animate.
+  @param {string|Function(element, options): Promise|Object} animation
+    Can either be:
 
-   - The animation's name
-   - A function performing the animation
-   - An object of CSS attributes describing the last frame of the animation (using kebeb-case property names)
-   @param {number} [options.duration=300]
-   The duration of the animation, in milliseconds.
-   @param {string} [options.easing='ease']
-   The timing function that controls the animation's acceleration.
+    - The animation's name
+    - A function performing the animation
+    - An object of CSS attributes describing the last frame of the animation (using kebeb-case property names)
+  @param {number} [options.duration=300]
+    The duration of the animation, in milliseconds.
+  @param {string} [options.easing='ease']
+    The timing function that controls the animation's acceleration.
 
-   See [MDN documentation](https://developer.mozilla.org/en-US/docs/Web/CSS/transition-timing-function)
-   for a list of pre-defined timing functions.
-   @return {Promise}
-   A promise for the animation's end.
-   @stable
-   */
+    See [MDN documentation](https://developer.mozilla.org/en-US/docs/Web/CSS/transition-timing-function)
+    for a list of pre-defined timing functions.
+  @return {Promise}
+    A promise for the animation's end.
+  @stable
+  */
   function animate(element, animation, options) {
     // If passed a selector, up.fragment.get() will prefer a match on the current layer.
     element = up.fragment.get(element)
@@ -221,27 +221,27 @@ up.motion = (function() {
   }
 
   /*-
-   Animates the given element's CSS properties using CSS transitions.
+  Animates the given element's CSS properties using CSS transitions.
 
-   Does not track the animation, nor does it finishes existing animations
-   (use `up.motion.animate()` for that). It does, however, listen to the motionController's
-   finish event.
+  Does not track the animation, nor does it finishes existing animations
+  (use `up.motion.animate()` for that). It does, however, listen to the motionController's
+  finish event.
 
-   @function animateNow
-   @param {Element|jQuery|string} element
-   The element to animate.
-   @param {Object} lastFrame
-   The CSS properties that should be transitioned to.
-   @param {number} [options.duration=300]
-   The duration of the animation, in milliseconds.
-   @param {string} [options.easing='ease']
-   The timing function that controls the animation's acceleration.
-   See [MDN documentation](https://developer.mozilla.org/en-US/docs/Web/CSS/transition-timing-function)
-   for a list of pre-defined timing functions.
-   @return {Promise}
-   A promise that fulfills when the animation ends.
-   @internal
-   */
+  @function animateNow
+  @param {Element|jQuery|string} element
+    The element to animate.
+  @param {Object} lastFrame
+    The CSS properties that should be transitioned to.
+  @param {number} [options.duration=300]
+    The duration of the animation, in milliseconds.
+  @param {string} [options.easing='ease']
+    The timing function that controls the animation's acceleration.
+    See [MDN documentation](https://developer.mozilla.org/en-US/docs/Web/CSS/transition-timing-function)
+    for a list of pre-defined timing functions.
+  @return {Promise}
+    A promise that fulfills when the animation ends.
+  @internal
+  */
   function animateNow(element, lastFrame, options) {
     options = { ...options, finishEvent: motionController.finishEvent }
     const cssTransition = new up.CSSTransition(element, lastFrame, options)
@@ -258,26 +258,26 @@ up.motion = (function() {
   }
 
   /*-
-   Completes [animations](/up.animate) and [transitions](/up.morph).
+  Completes [animations](/up.animate) and [transitions](/up.morph).
 
-   If called without arguments, all animations on the screen are completed.
-   If given an element (or selector), animations on that element and its children
-   are completed.
+  If called without arguments, all animations on the screen are completed.
+  If given an element (or selector), animations on that element and its children
+  are completed.
 
-   Animations are completed by jumping to the last animation frame instantly.
-   Promises returned by animation and transition functions instantly settle.
+  Animations are completed by jumping to the last animation frame instantly.
+  Promises returned by animation and transition functions instantly settle.
 
-   Emits the `up:motion:finish` event that is handled by `up.animate()`.
+  Emits the `up:motion:finish` event that is handled by `up.animate()`.
 
-   Does nothing if there are no animation to complete.
+  Does nothing if there are no animation to complete.
 
-   @function up.motion.finish
-   @param {Element|jQuery|string} [element]
-   The element around which to finish all animations.
-   @return {Promise}
-   A promise that fulfills when animations and transitions have finished.
-   @stable
-   */
+  @function up.motion.finish
+  @param {Element|jQuery|string} [element]
+    The element around which to finish all animations.
+  @return {Promise}
+    A promise that fulfills when animations and transitions have finished.
+  @stable
+  */
   function finish(element) {
     return motionController.finish(element)
   }
