f6465abae47d69e2941e9640930a9e6d13035679
Henning Koch <henning.koch@makandra.de>
2021-08-27 13:58:56 +0200

    Fix spec leaving browser in mocked quirks mode

--- spec/unpoly/framework_spec.js
+++ spec/unpoly/framework_spec.js
@@ -25,12 +25,15 @@ describe('up.framework', function() {
       it('returns false if the document is missing a DOCTYPE, triggering quirks mode', function() {
         // Cannot use spyOnProperty() for document.compatMode, as document does not return a property descriptor.
         let oldCompatMode = document.compatMode
+        expect(oldCompatMode).toEqual('CSS1Compat')
         try {
           // Quirks mode is "BackCompat". Standards mode is "CSS1Compat".
-          Object.defineProperty(document, 'compatMode', { value: 'BackCompat' })
+          Object.defineProperty(document, 'compatMode', { value: 'BackCompat', configurable: true })
           expect(up.framework.isSupported()).toBe(false)
         } finally {
-          document.compatMode = oldCompatMode
+          Object.defineProperty(document, 'compatMode', { value: oldCompatMode, configurable: true })
+          // Make sure we cleaned up correctly
+          expect(document.compatMode).toEqual(oldCompatMode)
         }
       })
 
