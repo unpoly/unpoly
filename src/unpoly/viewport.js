0853f84e84845ff346390070d44465cc3d599d4c
Henning Koch <henning.koch@makandra.de>
2022-05-07 23:05:08 +0200

    Kill IE11: We can now rely on non-interactive elements to have { tabindex: -1 } property

--- src/unpoly/viewport.js
+++ src/unpoly/viewport.js
@@ -281,19 +281,13 @@ up.viewport = (function() {
     return element === document.activeElement
   }
 
-  function isNativelyFocusable(element) {
-    // IE11: In modern browsers we can check if element.tabIndex >= 0.
-    // But IE11 returns 0 for all elements, including <div> etc.
-    return element.matches('a[href], button, textarea, input, select')
-  }
-
   function makeFocusable(element) {
     // (1) Element#tabIndex is -1 for all non-interactive elements,
     //     whether or not the element has an [tabindex=-1] attribute.
     // (2) Element#tabIndex is 0 for interactive elements, like links,
     //     inputs or buttons. [up-clickable] elements also get a [tabindex=0].
     //     to participate in the regular tab order.
-    if (!element.hasAttribute('tabindex') && !isNativelyFocusable(element)) {
+    if (!element.hasAttribute('tabindex') && element.tabIndex === -1) {
       element.setAttribute('tabindex', '-1')
 
       // A11Y: OK to hide the focus ring of a non-interactive element.
