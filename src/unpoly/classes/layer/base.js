ecd9b84d01bf64df69b1ce538a6d0c1a4bfab6f9
Henning Koch <henning.koch@makandra.de>
2022-06-10 16:09:03 +0200

    Fix: Clicking links twice will not update location when the browser history API is used in between (closes #388)

--- src/unpoly/classes/layer/base.js
+++ src/unpoly/classes/layer/base.js
@@ -680,7 +680,7 @@ up.Layer = class Layer extends up.Record {
   }
 
   set location(location) {
-    const previousLocation = this.savedLocation
+    const previousLocation = this.location
     location = up.history.normalizeURL(location)
 
     if (previousLocation !== location) {
