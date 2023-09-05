c36b27d517020643c7d3dc99ccfde047ff932286
Henning Koch <henning.koch@makandra.de>
2023-09-05 21:23:57 +0200

    Mangle props in up.OverlayFocus

--- src/unpoly/classes/overlay_focus.js
+++ src/unpoly/classes/overlay_focus.js
@@ -4,16 +4,16 @@ const u = up.util
 up.OverlayFocus = class OverlayFocus {
 
   constructor(layer) {
-    this.layer = layer
-    this.focusElement = this.layer.getFocusElement()
+    this._layer = layer
+    this._focusElement = this._layer.getFocusElement()
   }
 
   moveToFront() {
-    if (this.enabled) { return }
-    this.enabled = true
+    if (this._enabled) { return }
+    this._enabled = true
 
-    this.untrapFocus = up.on('focusin', event => this.onFocus(event))
-    this.unsetAttrs = e.setTemporaryAttrs(this.focusElement, {
+    this._untrapFocus = up.on('focusin', event => this._onFocus(event))
+    this._unsetAttrs = e.setTemporaryAttrs(this._focusElement, {
       // Make layer.element focusable.
       // It would be slightly nicer to give it [tabindex=-1] to make it focusable through JS,
       // but remove it from the keyboard tab sequence. However, then we would need additional
@@ -25,8 +25,8 @@ up.OverlayFocus = class OverlayFocus {
       // Tell modern screen readers to make all elements outside layer.element's subtree inert.
       'aria-modal': 'true'
     })
-    this.focusTrapBefore = e.affix(this.focusElement, 'beforebegin', 'up-focus-trap[tabindex=0]')
-    this.focusTrapAfter = e.affix(this.focusElement, 'afterend', 'up-focus-trap[tabindex=0]')
+    this._focusTrapBefore = e.affix(this._focusElement, 'beforebegin', 'up-focus-trap[tabindex=0]')
+    this._focusTrapAfter = e.affix(this._focusElement, 'afterend', 'up-focus-trap[tabindex=0]')
   }
 
   moveToBack() {
@@ -34,62 +34,62 @@ up.OverlayFocus = class OverlayFocus {
   }
 
   teardown() {
-    if (!this.enabled) { return }
-    this.enabled = false
+    if (!this._enabled) { return }
+    this._enabled = false
 
-    this.untrapFocus()
+    this._untrapFocus()
     // Remove [aria-modal] attribute to not confuse screen readers with multiple
     // mutually exclusive [aria-modal] layers.
-    this.unsetAttrs()
+    this._unsetAttrs()
 
-    this.focusTrapBefore.remove()
-    this.focusTrapAfter.remove()
+    this._focusTrapBefore.remove()
+    this._focusTrapAfter.remove()
   }
 
-  onFocus(event) {
+  _onFocus(event) {
     const { target } = event
 
     // (1) Ignore focus events triggered by this method.
     // (2) Ignore focus events within overlays by other libraries.
-    if (this.processingFocusEvent || up.layer.isWithinForeignOverlay(target)) {
+    if (this._processingFocusEvent || up.layer.isWithinForeignOverlay(target)) {
       return
     }
 
-    this.processingFocusEvent = true
+    this._processingFocusEvent = true
 
-    if (target === this.focusTrapBefore) {
+    if (target === this._focusTrapBefore) {
       // User shift-tabbed from the first focusable element in the overlay.
       // Focus pierced through the layer the the beginning.
       // We want to wrap around and focus the end of the overlay.
-      this.focusEnd()
-    } else if ((target === this.focusTrapAfter) || !this.layer.contains(target)) {
+      this._focusEnd()
+    } else if ((target === this._focusTrapAfter) || !this._layer.contains(target)) {
       // User tabbed from the last focusable element in the overlay
       // OR user moved their virtual cursor on an element outside the layer.
       // We want to to trap focus and focus the start of the overlay.
-      this.focusStart()
+      this._focusStart()
     }
 
-    this.processingFocusEvent = false
+    this._processingFocusEvent = false
   }
 
-  focusStart(focusOptions) {
+  _focusStart(focusOptions) {
     // Focusing the overlay element with its [role=dialog] will read out
     // "dialog field" in many screen readers.
-    up.focus(this.focusElement, focusOptions)
+    up.focus(this._focusElement, focusOptions)
   }
 
-  focusEnd() {
+  _focusEnd() {
     // The end will usually be the dismiss button, if there is one.
     // Otherwise it will be the last focusable element.
     // We focus on the box element since focusing on the layer container
     // would include the viewport, which is focusable due to scroll bars.
-    this.focusLastDescendant(this.layer.getBoxElement()) || this.focusStart()
+    this._focusLastDescendant(this._layer.getBoxElement()) || this._focusStart()
   }
 
-  focusLastDescendant(element) {
+  _focusLastDescendant(element) {
     // Don't use forEach since we need to break out of the loop with `return`
     for (let child of u.reverse(element.children)) {
-      if (up.viewport.tryFocus(child) || this.focusLastDescendant(child)) {
+      if (up.viewport.tryFocus(child) || this._focusLastDescendant(child)) {
         return true
       }
     }
