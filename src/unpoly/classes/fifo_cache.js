dec05832279bfffb7d622974af295814aea198f9
Henning Koch <henning.koch@makandra.de>
2023-09-05 17:58:47 +0200

    Mangle props in up.FIFOCache

--- src/unpoly/classes/fifo_cache.js
+++ src/unpoly/classes/fifo_cache.js
@@ -3,28 +3,28 @@ const u = up.util
 up.FIFOCache = class FIFOCache {
 
   constructor({ capacity = 10, normalizeKey = u.identity } = {}) {
-    this.map = new Map()
-    this.capacity = capacity
-    this.normalizeKey = normalizeKey
+    this._map = new Map()
+    this._capacity = capacity
+    this._normalizeKey = normalizeKey
   }
 
   get(key) {
-    key = this.normalizeKey(key)
-    return this.map.get(key)
+    key = this._normalizeKey(key)
+    return this._map.get(key)
   }
 
   set(key, value) {
-    if (this.map.size === this.capacity) {
-      let oldestKey = this.map.keys().next().value
-      this.map.delete(oldestKey)
+    if (this._map.size === this._capacity) {
+      let oldestKey = this._map.keys().next().value
+      this._map.delete(oldestKey)
     }
 
-    key = this.normalizeKey(key)
-    this.map.set(key, value)
+    key = this._normalizeKey(key)
+    this._map.set(key, value)
   }
 
   clear() {
-    this.map.clear()
+    this._map.clear()
   }
 
 }
