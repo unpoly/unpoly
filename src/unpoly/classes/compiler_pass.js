8ec846f6f4897fe008ef00221987b82e2fd144e3
Henning Koch <henning.koch@makandra.de>
2023-09-05 16:29:59 +0200

    Mangle props in up.CompilerPass

--- src/unpoly/classes/compiler_pass.js
+++ src/unpoly/classes/compiler_pass.js
@@ -7,112 +7,112 @@ up.CompilerPass = class CompilerPass {
     // (2) Default to the current layer in case the user manually compiles a detached element.
     layer ||= up.layer.get(root) || up.layer.current
 
-    this.root = root
-    this.compilers = compilers
-    this.layer = layer
-    this.data = data
-    this.dataMap = dataMap
+    this._root = root
+    this._compilers = compilers
+    this._layer = layer
+    this._data = data
+    this._dataMap = dataMap
 
-    this.meta = { layer, ...meta }
-    this.errors = []
+    this._meta = { layer, ...meta }
+    this._errors = []
   }
 
   run() {
     // If we're compiling a fragment in a background layer, we want
     // up.layer.current to resolve to that background layer, not the front layer.
-    this.layer.asCurrent(() => {
+    this._layer.asCurrent(() => {
 
       this.setCompileData()
 
-      for (let compiler of this.compilers) {
-        this.runCompiler(compiler)
+      for (let compiler of this._compilers) {
+        this._runCompiler(compiler)
       }
 
     })
 
-    if (this.errors.length) {
-      throw new up.CannotCompile('Errors while compiling', { errors: this.errors })
+    if (this._errors.length) {
+      throw new up.CannotCompile('Errors while compiling', { errors: this._errors })
     }
   }
 
   setCompileData() {
-    if (this.data) {
-      this.root.upCompileData = this.data
+    if (this._data) {
+      this._root.upCompileData = this._data
     }
 
-    if (this.dataMap) {
-      for (let selector in this.dataMap) {
-        for (let match of this.select(selector)) {
-          match.upCompileData = this.dataMap[selector]
+    if (this._dataMap) {
+      for (let selector in this._dataMap) {
+        for (let match of this._select(selector)) {
+          match.upCompileData = this._dataMap[selector]
         }
       }
     }
   }
 
-  runCompiler(compiler) {
-    const matches = this.selectOnce(compiler)
+  _runCompiler(compiler) {
+    const matches = this._selectOnce(compiler)
     if (!matches.length) { return }
 
     if (!compiler.isDefault) {
-      up.puts('up.hello()', 'Compiling %d× "%s" on %s', matches.length, compiler.selector, this.layer)
+      up.puts('up.hello()', 'Compiling %d× "%s" on %s', matches.length, compiler.selector, this._layer)
     }
 
     if (compiler.batch) {
-      this.compileBatch(compiler, matches)
+      this._compileBatch(compiler, matches)
     } else {
       for (let match of matches) {
-        this.compileOneElement(compiler, match)
+        this._compileOneElement(compiler, match)
       }
     }
 
     return up.migrate.postCompile?.(matches, compiler)
   }
 
-  compileOneElement(compiler, element) {
+  _compileOneElement(compiler, element) {
     const compileArgs = [element]
     // Do not retrieve and parse [up-data] unless the compiler function
     // expects a second argument. Note that we must pass data for an argument
     // count of 0, since then the function might take varargs.
     if (compiler.length !== 1) {
       const data = up.script.data(element)
-      compileArgs.push(data, this.meta)
+      compileArgs.push(data, this._meta)
     }
 
-    const result = this.applyCompilerFunction(compiler, element, compileArgs)
+    const result = this._applyCompilerFunction(compiler, element, compileArgs)
 
-    let destructorOrDestructors = this.destructorPresence(result)
+    let destructorOrDestructors = this._destructorPresence(result)
     if (destructorOrDestructors) {
       up.destructor(element, destructorOrDestructors)
     }
   }
 
-  compileBatch(compiler, elements) {
+  _compileBatch(compiler, elements) {
     const compileArgs = [elements]
     // Do not retrieve and parse [up-data] unless the compiler function
     // expects a second argument. Note that we must pass data for an argument
     // count of 0, since then the function might take varargs.
     if (compiler.length !== 1) {
       const dataList = u.map(elements, up.script.data)
-      compileArgs.push(dataList, this.meta)
+      compileArgs.push(dataList, this._meta)
     }
 
-    const result = this.applyCompilerFunction(compiler, elements, compileArgs)
+    const result = this._applyCompilerFunction(compiler, elements, compileArgs)
 
-    if (this.destructorPresence(result)) {
+    if (this._destructorPresence(result)) {
       up.fail('Compilers with { batch: true } cannot return destructors')
     }
   }
 
-  applyCompilerFunction(compiler, elementOrElements, compileArgs) {
+  _applyCompilerFunction(compiler, elementOrElements, compileArgs) {
     try {
       return compiler.apply(elementOrElements, compileArgs)
     } catch (error) {
-      this.errors.push(error)
+      this._errors.push(error)
       up.log.error('up.hello()', 'While compiling %o: %o', elementOrElements, error)
     }
   }
 
-  destructorPresence(result) {
+  _destructorPresence(result) {
     // Check if the result value looks like a destructor to filter out
     // unwanted implicit returns in CoffeeScript.
     if (u.isFunction(result) || (u.isArray(result) && (u.every(result, u.isFunction)))) {
@@ -120,12 +120,12 @@ up.CompilerPass = class CompilerPass {
     }
   }
 
-  select(selector) {
-    return up.fragment.subtree(this.root, u.evalOption(selector), { layer: this.layer } )
+  _select(selector) {
+    return up.fragment.subtree(this._root, u.evalOption(selector), { layer: this._layer } )
   }
 
-  selectOnce(compiler) {
-    let matches = this.select(compiler.selector)
+  _selectOnce(compiler) {
+    let matches = this._select(compiler.selector)
     return u.filter(matches, (element) => {
       let appliedCompilers = (element.upAppliedCompilers ||= new Set())
       if (!appliedCompilers.has(compiler)) {
