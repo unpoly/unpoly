2e276f1824d481b07660161cb7b59aaae7c9f0e9
Henning Koch <henning.koch@makandra.de>
2023-09-05 18:00:48 +0200

    Mangle props in up.FocusCapsule

--- src/unpoly/classes/focus_capsule.js
+++ src/unpoly/classes/focus_capsule.js
@@ -1,15 +1,15 @@
 up.FocusCapsule = class FocusCapsule {
 
   constructor(target, cursorProps) {
-    this.target = target
-    this.cursorProps = cursorProps
+    this._target = target
+    this._cursorProps = cursorProps
   }
 
   restore(layer, options) {
-    let rediscoveredElement = up.fragment.get(this.target, { layer })
+    let rediscoveredElement = up.fragment.get(this._target, { layer })
     if (rediscoveredElement) {
       // Firefox needs focus-related props to be set *before* we focus the element
-      up.viewport.copyCursorProps(this.cursorProps, rediscoveredElement)
+      up.viewport.copyCursorProps(this._cursorProps, rediscoveredElement)
       up.focus(rediscoveredElement, options)
 
       // Signals callers that we could restore.
