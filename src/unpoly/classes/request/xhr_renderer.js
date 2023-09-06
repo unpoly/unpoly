128f27dc56e981c1fe7147959484f500e27bbf72
Henning Koch <henning.koch@makandra.de>
2023-09-06 08:24:22 +0200

    Mangle props in up.Request.XHRRenderer

--- src/unpoly/classes/request/xhr_renderer.js
+++ src/unpoly/classes/request/xhr_renderer.js
@@ -6,98 +6,96 @@ const u = up.util
 up.Request.XHRRenderer = class XHRRenderer {
 
   constructor(request) {
-    this.request = request
+    this._request = request
   }
 
   buildAndSend(handlers) {
-    const xhr = this.request.xhr
+    const xhr = this._request.xhr
 
     // We copy params since we will modify them below.
     // This would confuse API clients and cache key logic in up.network.
-    this.params = u.copy(this.request.params)
+    this._params = u.copy(this._request.params)
 
     // IE11 explodes it we're setting an undefined timeout property
-    if (this.request.timeout) {
-      xhr.timeout = this.request.timeout
+    if (this._request.timeout) {
+      xhr.timeout = this._request.timeout
     }
 
     // The XMLHttpRequest method must be opened before we can add headers to it.
-    xhr.open(this.getMethod(), this.request.url)
+    xhr.open(this._getMethod(), this._request.url)
 
     // The { contentType } will be missing in case of a FormData payload.
     // In this case the browser will choose a content-type with MIME boundary,
     // like: multipart/form-data; boundary=----WebKitFormBoundaryHkiKAbOweEFUtny8
-    let contentType = this.getContentType()
+    let contentType = this._getContentType()
     if (contentType) {
       xhr.setRequestHeader('Content-Type', contentType)
     }
 
-    for (let headerName in this.request.headers) {
-      let headerValue = this.request.headers[headerName]
+    for (let headerName in this._request.headers) {
+      let headerValue = this._request.headers[headerName]
       xhr.setRequestHeader(headerName, headerValue)
     }
 
     Object.assign(xhr, handlers)
-    xhr.send(this.getPayload())
+    xhr.send(this._getPayload())
   }
 
-  getMethod() {
+  _getMethod() {
     // By default HTTP methods other than `GET` or `POST` will be converted into a `POST`
     // request and carry their original method as a `_method` parameter. This is to
     // [prevent unexpected redirect behavior](https://makandracards.com/makandra/38347)
     // if the server redirects with 302 (Rails default) instead of 303.
-    if (!this.method) {
-      this.method = this.request.method
-      if (this.request.wrapMethod && !this.request.will302RedirectWithGET()) {
-        this.method = up.protocol.wrapMethod(this.method, this.params)
-      }
+    let method = this._request.method
+    if (this._request.wrapMethod && !this._request.will302RedirectWithGET()) {
+      method = up.protocol.wrapMethod(method, this._params)
     }
-
-    return this.method
+    return method
   }
 
-  getContentType() {
-    this.finalizePayload()
-    return this.contentType
+  _getContentType() {
+    this._finalizePayload()
+    return this._contentType
   }
 
-  getPayload() {
-    this.finalizePayload()
-    return this.payload
+  _getPayload() {
+    this._finalizePayload()
+    return this._payload
   }
 
-  finalizePayload() {
-    this.payload = this.request.payload
-    this.contentType = this.request.contentType
+  _finalizePayload() {
+    this._payload = this._request.payload
+    this._contentType = this._request.contentType
 
     // (1) If a user sets { payload } we also expect them to set a { contentType }.
     //     In that case we don't change anything.
     // (2) We don't send a { contentType } or { payload } for GET requests.
-    if (!this.payload && this.request.allowsPayload()) {
+    if (!this._payload && this._request.allowsPayload()) {
 
       // Determine the effective Content-Type by looking at our params values.
-      if (!this.contentType) {
-        this.contentType = this.params.hasBinaryValues() ? CONTENT_TYPE_FORM_DATA : CONTENT_TYPE_URL_ENCODED
+      if (!this._contentType) {
+        this._contentType = this._params.hasBinaryValues() ? CONTENT_TYPE_FORM_DATA : CONTENT_TYPE_URL_ENCODED
       }
 
       // Serialize our payload
-      if (this.contentType === CONTENT_TYPE_FORM_DATA) {
+      if (this._contentType === CONTENT_TYPE_FORM_DATA) {
         // The effective Content-Type header will look like
         // multipart/form-data; boundary=----WebKitFormBoundaryHkiKAbOweEFUtny8
         // When we send a FormData payload the browser will automatically
         // chooose a boundary and set the payload.
-        this.contentType = null
-        this.payload = this.params.toFormData()
+        this._contentType = null
+        this._payload = this._params.toFormData()
       } else {
         // Only in form submissions %-encoded spaces are sent as a plus characater ("+")
-        this.payload = this.params.toQuery().replace(/%20/g, '+')
+        this._payload = this._params.toQuery().replace(/%20/g, '+')
       }
     }
   }
 
   static {
     u.memoizeMethod(this.prototype, [
-      'finalizePayload',
+      '_finalizePayload',
+      '_getMethod',
     ])
   }
 }
