97172db6bfc83d38cf57789cd58cd3fc9b2d2d63
Henning Koch <henning.koch@makandra.de>
2023-09-06 09:35:18 +0200

    Mangle props in up.Change.OpenLayer

--- src/unpoly/classes/change/open_layer.js
+++ src/unpoly/classes/change/open_layer.js
@@ -4,9 +4,9 @@ up.Change.OpenLayer = class OpenLayer extends up.Change.Addition {
 
   constructor(options) {
     super(options)
-    this.target = options.target
-    this.origin = options.origin
-    this.baseLayer = options.baseLayer
+    this._target = options.target
+    this._origin = options.origin
+    this._baseLayer = options.baseLayer
     // Don't extract too many @properties from @options, since listeners
     // to up:layer:open may modify layer options.
   }
@@ -17,49 +17,49 @@ up.Change.OpenLayer = class OpenLayer extends up.Change.Addition {
 
     return {
       mode: this.options.mode,
-      context: this.buildLayer().context,
+      context: this._buildLayer().context,
       origin: this.options.origin,
 
       // The target will always exist in the current page, since
       // we're opening a new layer that will match the target.
-      target: this.target,
+      target: this._target,
 
       // We associate this request to our base layer so up:request events may be emitted on something
       // more specific than the document. This will also abort this request when
       // `up.fragment.abort({ layer })` is called for the base layer.
-      layer: this.baseLayer,
+      layer: this._baseLayer,
 
       // We associate this request with the base layer's main element. This way the request
       // will be aborted if the base layer receives a major navigation, but not when a
       // minor fragment is updated.
-      fragments: u.compact([up.fragment.get(':main', { layer: this.baseLayer })]),
+      fragments: u.compact([up.fragment.get(':main', { layer: this._baseLayer })]),
 
       newLayer: true,
     }
   }
 
   execute(responseDoc, onApplicable) {
-    if (this.target === ':none') {
-      this.content = document.createElement('up-none')
+    if (this._target === ':none') {
+      this._content = document.createElement('up-none')
     } else {
-      this.content = responseDoc.select(this.target)
+      this._content = responseDoc.select(this._target)
     }
 
-    if (!this.content || this.baseLayer.isClosed()) {
+    if (!this._content || this._baseLayer.isClosed()) {
       // An error message will be chosen by up.Change.FromContent
       throw new up.CannotMatch()
     }
 
     onApplicable()
-    up.puts('up.render()', `Opening element "${this.target}" in new overlay`)
+    up.puts('up.render()', `Opening element "${this._target}" in new overlay`)
 
-    if (this.emitOpenEvent().defaultPrevented) {
+    if (this._emitOpenEvent().defaultPrevented) {
       // We cannot use @abortWhenLayerClosed() here,
       // because the layer is not even in the stack yet.
       throw new up.Aborted('Open event was prevented')
     }
 
-    this.layer = this.buildLayer()
+    this.layer = this._buildLayer()
 
     // (1) Make sure that the baseLayer layer doesn't already have a child layer.
     //     This cannot be prevented with { peel: false }, as the layer stack must be a sequence,
@@ -68,24 +68,24 @@ up.Change.OpenLayer = class OpenLayer extends up.Change.Addition {
     // (2) Only restore the base layer's history if the new overlay does not add one of its own.
     //     Otherwise we would add an intermediate history entries when swapping overlays
     //     with { layer: 'swap' } (issue #397).
-    this.baseLayer.peel({ history: !this.layer.history })
+    this._baseLayer.peel({ history: !this.layer.history })
 
     // Don't wait for peeling to finish. Change the stack sync so there is no state
     // when the new overlay is scheduled to be pushed, but not yet in the stack.
     up.layer.stack.push(this.layer)
 
-    this.layer.createElements(this.content)
+    this.layer.createElements(this._content)
 
     this.layer.setupHandlers()
 
     // Change history before compilation, so new fragments see the new location.
-    this.handleHistory()
+    this._handleHistory()
 
     // Remember where the element came from to support up.reload(element).
-    this.setReloadAttrs({ newElement: this.content, source: this.options.source })
+    this.setReloadAttrs({ newElement: this._content, source: this.options.source })
 
     // Unwrap <noscript> tags
-    responseDoc.finalizeElement(this.content)
+    responseDoc.finalizeElement(this._content)
 
     // Compile the entire layer, not just the user content.
     // E.g. [up-dismiss] in the layer elements needs to go through a macro.
@@ -104,20 +104,20 @@ up.Change.OpenLayer = class OpenLayer extends up.Change.Addition {
 
     // Don't wait for the open animation to finish.
     // Otherwise a popup would start to open and only reveal itself after the animation.
-    this.handleScroll()
+    this._handleScroll()
 
-    this.renderResult = new up.RenderResult({
+    this._renderResult = new up.RenderResult({
       layer: this.layer,
-      fragments: [this.content],
-      target: this.target,
+      fragments: [this._content],
+      target: this._target,
     })
 
-    this.renderResult.finished = this.finish()
+    this._renderResult.finished = this._finish()
 
     // Emit up:layer:opened to indicate that the layer was opened successfully.
     // This is a good time for listeners to manipulate the overlay optics.
     this.layer.opening = false
-    this.emitOpenedEvent()
+    this._emitOpenedEvent()
 
     // In case a listener to up:layer:opened immediately dimisses the new layer,
     // reject the promise returned by up.layer.open().
@@ -128,10 +128,10 @@ up.Change.OpenLayer = class OpenLayer extends up.Change.Addition {
     //     layer = await up.layer.open(...)
     //
     // Don't wait to animations to finish:
-    return this.renderResult
+    return this._renderResult
   }
 
-  async finish() {
+  async _finish() {
     await this.layer.startOpenAnimation()
 
     // Don't change focus if the layer has been closed while the animation was running.
@@ -139,13 +139,13 @@ up.Change.OpenLayer = class OpenLayer extends up.Change.Addition {
 
     // A11Y: Place the focus on the overlay element and setup a focus circle.
     // However, don't change focus if the layer has been closed while the animation was running.
-    this.handleFocus()
+    this._handleFocus()
 
     // Resolve the RenderResult#finished promise for callers that need to know when animations are done.
-    return this.renderResult
+    return this._renderResult
   }
 
-  buildLayer() {
+  _buildLayer() {
     // We need to mark the layer as { opening: true } so its topmost swappable element
     // does not resolve from the :layer pseudo-selector. Since :layer is a part of
     // up.fragment.config.mainTargets and :main is a part of fragment.config.autoHistoryTargets,
@@ -159,11 +159,11 @@ up.Change.OpenLayer = class OpenLayer extends up.Change.Addition {
     return up.layer.build(buildOptions, beforeNew)
   }
 
-  handleHistory() {
+  _handleHistory() {
     // If the layer is opened with { history } auto, the new overlay will from now
     // on have visible history *if* its initial fragment has auto-history.
     if (this.layer.history === 'auto') {
-      this.layer.history = up.fragment.hasAutoHistory(this.content)
+      this.layer.history = up.fragment.hasAutoHistory(this._content)
     }
 
     this.layer.parent.saveHistory()
@@ -174,22 +174,22 @@ up.Change.OpenLayer = class OpenLayer extends up.Change.Addition {
     this.layer.updateHistory(this.options)
   }
 
-  handleFocus() {
-    this.baseLayer.overlayFocus?.moveToBack()
+  _handleFocus() {
+    this._baseLayer.overlayFocus?.moveToBack()
     this.layer.overlayFocus.moveToFront()
 
     const fragmentFocus = new up.FragmentFocus({
-      fragment: this.content,
+      fragment: this._content,
       layer: this.layer,
       autoMeans: ['autofocus', 'layer']
     })
     fragmentFocus.process(this.options.focus)
   }
 
-  handleScroll() {
+  _handleScroll() {
     const scrollingOptions = {
       ...this.options,
-      fragment: this.content,
+      fragment: this._content,
       layer: this.layer,
       autoMeans: ['hash', 'layer']
     }
@@ -197,7 +197,7 @@ up.Change.OpenLayer = class OpenLayer extends up.Change.Addition {
     scrolling.process(this.options.scroll)
   }
 
-  emitOpenEvent() {
+  _emitOpenEvent() {
     // The initial up:layer:open event is emitted on the document, since the layer
     // element has not been attached yet and there is no obvious element it should
     // be emitted on. We don't want to emit it on @layer.parent.element since users
@@ -205,16 +205,16 @@ up.Change.OpenLayer = class OpenLayer extends up.Change.Addition {
     //
     // There is no @layer.onOpen() handler to accompany the DOM event.
     return up.emit('up:layer:open', {
-      origin: this.origin,
-      baseLayer: this.baseLayer, // sets up.layer.current
+      origin: this._origin,
+      baseLayer: this._baseLayer, // sets up.layer.current
       layerOptions: this.options,
       log: "Opening new overlay"
     })
   }
 
-  emitOpenedEvent() {
+  _emitOpenedEvent() {
     this.layer.emit('up:layer:opened', {
-      origin: this.origin,
+      origin: this._origin,
       callback: this.layer.callback('onOpened'),
       log: `Opened new ${this.layer}`
     }
