54f9bbe09321b2e921c959210f96b53f35b896e3
Henning Koch <henning.koch@makandra.de>
2024-03-31 22:42:36 +0200

    Simplify waitMicroTasks()

--- spec/helpers/wait.js
+++ spec/helpers/wait.js
@@ -11,24 +11,11 @@ window.wait = waitTime
 
 jasmine.waitTime = waitTime
 
-// function waitMicrotasks(count = 1) {
-//   // Awaiting this initial promise is already the first microtask
-//   let promise = Promise.resolve()
-//
-//   for (let i = 1; i < count; i++) {
-//     promise = promise.then(() => {})
-//   }
-//
-//   return promise
-// }
-
-
 async function waitMicrotasks(count = 1) {
   for (let i = 0; i < count; i++) {
     await Promise.resolve()
   }
 }
-
 
 jasmine.waitMicrotasks = waitMicrotasks
 
