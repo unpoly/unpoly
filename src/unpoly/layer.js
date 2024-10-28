4634e7fbacea1f338383365ef5ba30a2fc230ab3
Henning Koch <henning.koch@makandra.de>
2024-10-28 11:13:58 +0100

    [layer] Allow comma-separator for { dismissable }

--- src/unpoly/layer.js
+++ src/unpoly/layer.js
@@ -771,7 +771,7 @@ up.layer = (function() {
     See [customizing dismiss controls](/closing-overlays#customizing-dismiss-controls)
     for details.
 
-    You may enable multiple dismiss controls by passing a space-separated string.
+    You may enable multiple dismiss controls by separating values with a space or comma character.
 
     Passing `true` or `false` will enable or disable all dismiss controls.
 
