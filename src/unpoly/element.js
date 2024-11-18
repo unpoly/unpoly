6a62d5570df566d55a9cf2da2b89a35498987337
Henning Koch <henning.koch@makandra.de>
2024-11-18 16:27:43 +0100

    [fragment] Add up:template:use to hook up custom template engines

--- src/unpoly/element.js
+++ src/unpoly/element.js
@@ -849,7 +849,7 @@ up.element = (function() {
     if (nodes.length === 1 && u.isElementLike(nodes[0])) {
       return nodes[0]
     } else {
-      up.fail('Expected a single element, but got %o', nodes)
+      up.fail('Expected a single element, but got %d elements', nodes.length)
     }
   }
 
