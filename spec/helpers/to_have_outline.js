20dc8377c7177b7970a0099d07a27f3aa448e5db
Henning Koch <henning.koch@makandra.de>
2024-12-04 10:49:55 +0100

    [specs] Matcher toHaveOutline() also checks for transparent color

--- spec/helpers/to_have_outline.js
+++ spec/helpers/to_have_outline.js
@@ -1,11 +1,18 @@
+function isTransparentColor(value) {
+  return value === 'transparent' || value === 'rgba(0, 0, 0, 0)' || value === 'rgba(0,0,0,0)'
+}
+
 beforeEach(function() {
   jasmine.addMatchers({
     toHaveOutline: function(util, customEqualityTesters) {
       return {
         compare: function(element) {
           element = up.element.get(element)
+          const widthNumber = up.element.styleNumber(element, 'outline-width')
+          const colorString = up.element.style(element, 'outline-color')
+
           return {
-            pass: up.element.styleNumber(element, 'outline-width') > 0
+            pass: widthNumber > 0 && !isTransparentColor(colorString)
           }
         }
       }
