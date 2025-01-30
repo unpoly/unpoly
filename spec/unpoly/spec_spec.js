ba54a8cfbb27ab33a4b5c14f8e542db125c19971
Henning Koch <henning.koch@makandra.de>
2025-01-30 16:43:25 +0100

    [specs] Decaffeinate spec_spec

---
+++ spec/unpoly/spec_spec.js
@@ -0,0 +1,10 @@
+const u = up.util
+const $ = jQuery
+
+describe('Jasmine setup', function() {
+  it('does not consider a jQuery collection to be equal to its contained element (which is what jasmine-jquery does and which makes out expectations too soft)', function() {
+    const element = document.createElement('div')
+    const $element = $(element)
+    return expect($element).not.toEqual(element)
+  })
+})
