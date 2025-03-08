173e7db9abfdbc54c54fe23ebb7fb0444691223e
Henning Koch <henning.koch@makandra.de>
2025-03-08 17:22:18 +0100

    Make eslint happy

--- spec/app.js
+++ spec/app.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 const opn = require('opn')
 
 const { execSync } = require('child_process')
@@ -8,20 +8,20 @@ const cwd = execSync('pwd').toString().trim()
 
 app.use(express.static(cwd))
 
-const PORT = process.env.PORT || 4000;
-const HOST = process.env.HOST || 'localhost';
+const PORT = process.env.PORT || 4000
+const HOST = process.env.HOST || 'localhost'
 const URL = `http://${HOST}:${PORT}`
 
 app.listen(PORT, HOST, function(){
   console.log(`Unpoly specs serving on ${URL}.`)
   console.log("Press CTRL+C to quit.")
   opn(URL)
-});
+})
 
 app.get('/', function(req, res){
-  res.sendFile(cwd + '/spec/menu.html');
-});
+  res.sendFile(cwd + '/spec/menu.html')
+})
 
 app.get('/specs', function(req, res){
-  res.sendFile(cwd + '/spec/runner.html');
-});
+  res.sendFile(cwd + '/spec/runner.html')
+})
