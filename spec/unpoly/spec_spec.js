089c1271c766127d56e57be3351fa2595371ca90
Henning Koch <henning.koch@makandra.de>
2025-06-23 11:29:16 +0200

    [specs] Test that `await wait()` pushes path long promise chain callbacks

--- spec/unpoly/spec_spec.js
+++ spec/unpoly/spec_spec.js
@@ -1,10 +1,35 @@
 const u = up.util
 const $ = jQuery
 
-describe('Jasmine setup', function() {
-  it('does not consider a jQuery collection to be equal to its contained element (which is what jasmine-jquery does and which makes out expectations too soft)', function() {
-    const element = document.createElement('div')
-    const $element = $(element)
-    expect($element).not.toEqual(element)
+fdescribe('specs', function() {
+
+  describe('toEqual() matcher', function() {
+    it('does not consider a jQuery collection to be equal to its contained element (which is what jasmine-jquery does and which makes out expectations too soft)', function() {
+      const element = document.createElement('div')
+      const $element = $(element)
+      expect($element).not.toEqual(element)
+    })
   })
+
+  describe('wait()', function() {
+
+    describe('without arguments', function() {
+
+      it('waits until a long promise chain has fired callbacks', async function() {
+        let callback = jasmine.createSpy('callback')
+
+        let promise = Promise.resolve()
+        for (let i = 0; i < 100; i++) {
+          promise = promise.then(Promise.resolve())
+        }
+        promise.then(callback)
+        expect(callback).not.toHaveBeenCalled()
+
+        await wait()
+        expect(callback).toHaveBeenCalled()
+      })
+
+    })
+  })
+
 })
