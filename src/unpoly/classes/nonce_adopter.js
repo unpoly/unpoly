2ceb5ecb907e2a11e10bfe6b7c95611c281db35a
Henning Koch <henning.koch@makandra.de>
2026-02-09 13:55:29 +0100

    More revision

--- src/unpoly/classes/nonce_adopter.js
+++ src/unpoly/classes/nonce_adopter.js
@@ -1,97 +1,6 @@
 const e = up.element
 const u = up.util
 
-class CallbackItem {
-
-  readNonce() {
-    return this._getCallback().nonce
-  }
-
-  writeNonce(nonce) {
-    let callback = this._getCallback()
-    callback.nonce = nonce
-    this.writeCallbackString(this._attrName, callback.toString())
-  }
-
-  _getCallback() {
-    return this._cacheCallback ||= up.NonceableCallback.fromString(this.readCallbackString())
-  }
-
-  disable() {
-    this.writeCallbackString('/* disabled */')
-  }
-
-}
-
-class AttributeItem extends CallbackItem {
-
-  constructor(element, attrName) {
-    super()
-    this._element = element
-    this._attrName = attrName
-  }
-
-  readCallbackString() {
-    return this._element.getAttribute(this._attrName)
-  }
-
-  writeCallbackString(string) {
-    this._element.setAttribute(this._attrName, string)
-  }
-
-  asEvent() {
-    return { element: this._object, attribute: this._attrName }
-  }
-
-}
-
-
-class ObjectPropertyItem {
-
-  constructor(object, key) {
-    this._object = object
-    this._key = key
-  }
-
-  readCallbackString() {
-    return this._object[this._key]
-  }
-
-  writeCallbackString(string) {
-    this._object[this._key] = string
-  }
-
-  asEvent() {
-    return { object: this._object, key: this._key }
-  }
-
-}
-
-class ScriptElementItem {
-
-  constructor(element) {
-    this._element = element
-  }
-
-  readNonce() {
-    return this._element.nonce
-  }
-
-  writeNonce(nonce) {
-    this._element.nonce = nonce
-  }
-
-  asEvent() {
-    return { element: this._element }
-  }
-
-  disable() {
-    this._element.type = 'up-disabled-script'
-  }
-
-}
-
-
 const STRATEGY_FNS = {
   pass: () => false,
   block: () => true,
@@ -111,29 +20,6 @@ up.ScriptAdopter = class ScriptAdopter {
     this._adoptAttributeCallbacks(root)
   }
 
-  _adoptScriptElements(root) {
-    let scriptElementStrategy = this._resolveStrategy(up.script.config.allow.scriptElements)
-
-    for (let script of up.script.findScripts(root)) {
-      let item = new ScriptElementItem(script)
-      this._processItem(item, scriptElementStrategy)
-    }
-  }
-
-  _adoptAttributeCallbacks(root) {
-    let attributeStrategy = this._resolveStrategy(up.script.config.allow.attributeCallbacks)
-
-    // TODO: It is weird that we keep a list of callback options in RenderOptions, but the list of nonceable attrs is in up.script
-    // TODO: Maybe at least move them together?
-    for (let attribute of up.script.config.nonceableAttributes) {
-      let matches = e.subtree(root, e.attrSelector(attribute))
-      for (let element of matches) {
-        let item = new AttributeItem(element, attribute)
-        this._processItem(item, attributeStrategy)
-      }
-    }
-  }
-
   adoptRenderOptionsFromHeader(object) {
     let objectStrategy = this._resolveStrategy(up.script.config.allow.headerCallbacks)
 
@@ -146,10 +32,38 @@ up.ScriptAdopter = class ScriptAdopter {
     }
   }
 
+  _adoptScriptElements(root) {
+    let scriptElementStrategy = this._resolveStrategy(up.script.config.allow.scriptElements)
+
+    for (let script of up.script.findScripts(root)) {
+      let item = new ScriptElementItem(script)
+      this._processItem(item, scriptElementStrategy)
+    }
+  }
+
+  _adoptAttributeCallbacks(root) {
+    let attributeStrategy = this._resolveStrategy(up.script.config.allow.attributeCallbacks)
+
+    // TODO: It is weird that we keep a list of callback options in RenderOptions, but the list of nonceable attrs is in up.script? Maybe at least move them together?
+    for (let attribute of up.script.config.nonceableAttributes) {
+      let matches = e.subtree(root, e.attrSelector(attribute))
+      for (let element of matches) {
+        let item = new AttributeItem(element, attribute)
+        this._processItem(item, attributeStrategy)
+      }
+    }
+  }
+
   _processItem(item, strategy) {
     if (strategy(item)) {
-      item.disable()
-    } else if (this._hasValidNonce(item)) {
+      this._tryAdoptNonce(item)
+    } else {
+      item.block()
+    }
+  }
+
+  _tryAdoptNonce(item) {
+    if (this._hasValidNonce(item)) {
       item.writeNonce(this._pageNonce)
     }
   }
@@ -179,6 +93,85 @@ up.ScriptAdopter = class ScriptAdopter {
 
   _isStrictDynamicCSP() {
     return this._response?.cspInfo.declaration.includes("'strict-dynamic'")
+  }
+
+}
+
+class StringCallbackItem {
+
+  readNonce() {
+    return this._getCallback().nonce
+  }
+
+  writeNonce(nonce) {
+    let callback = this._getCallback()
+    callback.nonce = nonce
+    this.writeCallbackString(this._attrName, callback.toString())
+  }
+
+  _getCallback() {
+    return this._cacheCallback ||= up.NonceableCallback.fromString(this.readCallbackString())
+  }
+
+  block() {
+    this.writeCallbackString('/* blocked */')
+  }
+
+}
+
+class AttributeItem extends StringCallbackItem {
+
+  constructor(element, attribute) {
+    super()
+    this.element = element
+    this.attribute = attribute
+  }
+
+  readCallbackString() {
+    return this._element.getAttribute(this.attribute)
+  }
+
+  writeCallbackString(string) {
+    this.element.setAttribute(this.attribute, string)
+  }
+
+
+}
+
+class ObjectPropertyItem extends StringCallbackItem {
+
+  constructor(object, key) {
+    super()
+    this.object = object
+    this.key = key
+  }
+
+  readCallbackString() {
+    return this.object[this.key]
+  }
+
+  writeCallbackString(string) {
+    this.object[this.key] = string
+  }
+
+}
+
+class ScriptElementItem {
+
+  constructor(element) {
+    this.element = element
+  }
+
+  readNonce() {
+    return this.element.nonce
+  }
+
+  writeNonce(nonce) {
+    this.element.nonce = nonce
+  }
+
+  block() {
+    this.element.type = 'up-blocked-script'
   }
 
 }
